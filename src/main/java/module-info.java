8933d2432ccc03b67a651011d02d40383130d24d
charl00ty <charlestripplhhs@gmail.com>
2022-10-23 03:40:29 -0700

    Added java, fxml, and media files for the project. Unfinished.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.group25.cse360groupproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.sdpizza.groupproject to javafx.fxml;
-    exports com.sdpizza.groupproject;
-}
