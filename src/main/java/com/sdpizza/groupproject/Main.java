ff88a355db91d4396735b944e14d20fc569ab6ab
Liam Petrie <liamdpetrie@vivaldi.net>
2022-11-05 22:24:57 -0700

    Fixed disable button and labels

--- src/main/java/com/sdpizza/groupproject/Main.java
+++ src/main/java/com/sdpizza/groupproject/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         fxmlLoader =
                 new FXMLLoader(
-                    Main.class.getResource("home-view.fxml")
+                    Main.class.getResource("orders.fxml")
                 );
         /* Setting the controller for the application here
            NOTE: You may run into weird behavior if you are using the
