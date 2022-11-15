4a0f8041bd782ff31f776ac3fe524a4d879d90ca
FigoFeras1 <figoferas@hotmail.com>
2022-11-14 17:20:41 -0700

    minor changes

---
+++ src/main/java/com/sdpizza/groupproject/controller/util/OrderCell.java
@@ -0,0 +1,8 @@
+package com.sdpizza.groupproject.controller.util;
+
+import com.sdpizza.groupproject.entity.model.Order;
+import javafx.scene.control.ListCell;
+
+public class OrderCell extends ListCell<Order> {
+
+}
